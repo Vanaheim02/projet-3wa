9e36d0f746114afbbb1aeced7f06bd2c34101e17
samson.svein@gmail.com <samson.svein@gmail.com>
2024-02-15 23:07:55 +0100

    Cohérence noms BDD / noms Controler/route/db + fix connexion BDD

--- src/databases/init.db.js
+++ src/databases/init.db.js
@@ -2,10 +2,11 @@ import mysql from "mysql";
 
 const pool = mysql.createPool({
   connectionLimit: 10000,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || "3306",
+  user: process.env.DB_USER || "admin",
+  password: process.env.DB_PASS || "admin",
+  database: process.env.DB_NAME || "projet-3wa",
 });
 
 const query = (sql, values = []) => {
--- src/routes/init.routes.js
+++ src/routes/init.routes.js
@@ -4,17 +4,17 @@
 // import initMylistRoutes from './mylist.routes.js':
 // import initPlateformRoutes from './plateform.routes.js';
 // import initUser from './user.routes.js';
-import initUserRoutes from './user.routes.js';
+import initUsersRoutes from './users.routes.js';
 
 const initRoutes = (app) => {
-    
+
 //   initCalendarRoutes(app);
 //   initCategoryRoutes(app);
 //   initMyaccountRoutes(app);
 //   initUsersRoutes(app);
 //   initMylistRoutes(app);
 //   initPlateformRoutes(app);
-    initUserRoutes(app);
+    initUsersRoutes(app);
 };
 
 export default initRoutes;
